340936e86eb3775380dcfe253bdd4eb90b89cbe2
Dan Spratling <daniel.spratling@toumetis.com>
2020-02-20 11:39:20 +0000

    Plugin can now make better use of the data.
    Can accept strings, objects with or without endpoints.
    Also improves reuse of code & naming.

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -4,22 +4,15 @@ exports.sourceNodes = (
   configOptions
 ) => {
   const { createNode } = actions
-  const { urls } = configOptions
+  const { apis } = configOptions
+
   // Gatsby adds a configOption that's not needed for this plugin, delete it
   delete configOptions.plugins
 
-  //strips special characters and makes string camelcase
-  const customFormat = str => {
-    return str
-      .replace(/^.*\/\/[^\/]+/, '') //Removes domain
-      .replace(/(?:^\w|[A-Z]|\b\w)/g, word => word.toUpperCase()) //Capitalizes strings
-      .replace(/\//g, '') //Removes slashes
-      .replace(/\-+/g, '') //Removes hyphens
-      .replace(/\s+/g, '') //Removes spaces
-  }
+  const sources = []
 
   // Helper function that processes a result to match Gatsby's node structure
-  const processResult = ({ result, endpoint, prefix = `RestApi` }) => {
+  const processResult = ({ result, endpoint, prefix }) => {
     const nodeId = createNodeId(`${endpoint}-${result.id}`)
     const nodeContent = JSON.stringify(result)
     const nodeData = Object.assign({}, result, {
@@ -37,79 +30,88 @@ exports.sourceNodes = (
     return nodeData
   }
 
-  // Helper function to fetch data
-  const fetchData = async (url) => {
-    const response = await fetch(`${url}`, {})
-    return await response.json()
+  const appendSources = ({ url, endpoint, prefix }) => {
+    sources.push(
+      fetchData(url).then(data => {
+        if (Array.isArray(data)) { /* if fetchData returns multiple results */
+          data.forEach(result => {
+            const nodeData = processResult({
+              result,
+              endpoint,
+              prefix
+            })
+            createNode(nodeData)
+          })
+        } else { // Otherwise a single result has been returned
+          const nodeData = processResult({
+            result: data,
+            endpoint,
+            prefix
+          })
+          createNode(nodeData)
+        }
+      }).catch(error => console.log(error))
+    )
   }
 
+  apis.forEach(api => {
 
-  const sources = []
-  urls.forEach(url => {
+    /* check if the api request is an object with parameters */
+    if (typeof (api) === "object") {
+      const { prefix, baseUrl, endpoints } = api
 
-    /** if url is a string, just process like normal */
-    if (typeof (url) === "object") {
-      const { prefix, baseUrl, endpoints } = url
-
-      /* Add some error logging if config options are mising */
+      /* Add some error logging if required config options are mising */
       if (!baseUrl) {
         console.log('\x1b[31m')
-        console.error("error gatsby-source-rest-api option requires baseUrl")
-      }
-
-      if (!endpoints) {
-        console.log('\x1b[31m')
-        console.error("error gatsby-source-rest-api option requires at least 1 endpoint")
-      }
-
-      if (!baseUrl || !endpoints) {
+        console.error("error gatsby-source-rest-api option requires the baseUrl parameter")
         console.log('')
         return
       }
 
-      /* proceed if we have the correct params available */
-      endpoints.forEach(endpoint => {
-        const url = baseUrl[baseUrl.length - 1] === '/' ? `${baseUrl}${endpoint}` : `${baseUrl}/${endpoint}`
+      /* object is used and endpoints are set */
+      if (endpoints && endpoints.length) {
+        endpoints.forEach(endpoint => {
+          appendSources({
+            url: baseUrl[baseUrl.length - 1] === '/' ? `${baseUrl}${endpoint}` : `${baseUrl}/${endpoint}`,
+            endpoint,
+            prefix
+          })
+        })
+        return
+      }
 
-        sources.push(
-          fetchData(url).then(data => {
-            if (Array.isArray(data)) { /* if fetchData returns multiple results */
-              data.forEach(result => {
-                const nodeData = processResult({
-                  result,
-                  endpoint,
-                  prefix
-                })
-                createNode(nodeData)
-              })
-            } else { // Otherwise a single result has been returned
-              const nodeData = processResult({
-                result: data,
-                endpoint,
-                prefix
-              })
-              createNode(nodeData)
-            }
-          }).catch(error => console.log(error))
-        )
+      /* object is used but no endpoints are set */
+      appendSources({
+        url: baseUrl,
+        endpoint: baseUrl,
+        prefix
       })
       return
     }
 
-    /* The default simply expects a url as a string */
-    sources.push(
-      fetchData(url).then(data => {
-        data.forEach(result => {
-          const nodeData = processResult({
-            result,
-            endpoint: url
-          })
-          createNode(nodeData)
-        })
-      })
-    )
+    /* The default simply expects a api url as a string and no other options */
+    appendSources({
+      url: api,
+      endpoint: api,
+      prefix: "MultiApiSource"
+    })
   })
 
-
   return Promise.all(sources)
+}
+
+// Helper function to fetch data
+const fetchData = async (url) => {
+  const response = await fetch(`${url}`, {})
+  return await response.json()
+}
+
+//strips special characters and makes string camelcase
+const customFormat = str => {
+  return str
+    .replace(/^.*\/\/[^\/]+/, '') //Removes domain
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, word => word.toUpperCase()) //Capitalizes strings
+    .replace(/\//g, '') //Removes slashes
+    .replace(/\-+/g, '') //Removes hyphens
+    .replace(/\s+/g, '') //Removes spaces
 }
